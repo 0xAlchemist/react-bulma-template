0121a0ee5cdeb7f6a185e741ba5c7635f44862ad
Eric Breuers <eric@ebdigital.ca>
2020-10-20 09:26:38 -0700

    Restore OG Bulma theme styles and upgrade webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
-const HTMLWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+  mode: "development",
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "./dist"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
   module: {
     rules: [
       {
+        // JS(X)
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
@@ -11,21 +31,36 @@ module.exports = {
         },
       },
       {
+        // HTML
         test: /\.html$/,
         use: {
           loader: "html-loader",
         },
       },
       {
+        // Styles
         test: /\.s[ac]ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        // Images
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        // Fonts and SVGs
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
       },
     ],
   },
   plugins: [
-    new HTMLWebPackPlugin({
-      template: "./public/index.html",
+    new HTMLWebpackPlugin({
+      title: "0xAlchemist's Webpack Boilerplate",
+      template: path.resolve(__dirname, "./public/index.html"),
       filename: "index.html",
     }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
