aaca90050e1d3c29b5b0ed03280f0ae4afd58e3e
Eric Breuers <eric@ebdigital.ca>
2020-10-20 16:14:22 -0700

    Add bulma components and change default colour scheme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-import Hero from "./components/bulma/Hero";
+import { Hero, Container, Heading } from "react-bulma-components";
 
 import "./App.scss";
 
 const App = () => (
-  <div>
-    <Hero />
-  </div>
+  <Hero color="primary" size="fullheight">
+    <Hero.Body className="has-text-centered">
+      <Container>
+        <Heading>0xAlchemist's</Heading>
+        <Heading subtitle size={3}>
+          Wicked Cool React Boilerplate
+        </Heading>
+      </Container>
+    </Hero.Body>
+  </Hero>
 );
 
 export default App;
